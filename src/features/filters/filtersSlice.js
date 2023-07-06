5bda64a79599f16c54085cb7779ed73ede4bbf29
anakinsonone <akkhi.sonone@gmail.com>
2023-07-06 23:38:27 +0530

    Add slice to manage filters state

---
+++ src/features/filters/filtersSlice.js
@@ -0,0 +1,23 @@
+const initialState = {
+  status: 'All',
+  colors: [],
+}
+
+export default function filtersReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'filters/statusFilterChanged': {
+      return {
+        ...state,
+        status: action.payload,
+      }
+    }
+    case 'filters/colorFilterChanged': {
+      return {
+        ...state,
+        colors: [...state.colors, action.payload],
+      }
+    }
+    default:
+      return state
+  }
+}
