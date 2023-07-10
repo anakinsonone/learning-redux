62b52e2624d828f8e2d7f7ff5a5b1441221a06d5
anakinsonone <akkhi.sonone@gmail.com>
2023-07-10 23:06:16 +0530

    Enumerate status filters and add functionality to filterReducer

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -1,5 +1,11 @@
+export const StatusFilters = {
+  All: 'all',
+  Active: 'active',
+  Completed: 'completed',
+}
+
 const initialState = {
-  status: 'All',
+  status: StatusFilters.All,
   colors: [],
 }
 
@@ -12,9 +18,29 @@ export default function filtersReducer(state = initialState, action) {
       }
     }
     case 'filters/colorFilterChanged': {
-      return {
-        ...state,
-        colors: [...state.colors, action.payload],
+      let { color, changeType } = action.payload
+      const { colors } = state
+
+      switch (changeType) {
+        case 'added': {
+          if (colors.includes(color)) {
+            return state
+          }
+          return {
+            ...state,
+            colors: state.colors.concat(color),
+          }
+        }
+        case 'removed': {
+          return {
+            ...state,
+            color: state.colors.filter(
+              (existingColor) => existingColor !== color
+            ),
+          }
+        }
+        default:
+          return state
       }
     }
     default:
