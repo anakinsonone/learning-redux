3db9d5214ca518bbe9ff09f34aa397b9eaa442ae
anakinsonone <akkhi.sonone@gmail.com>
2023-07-13 01:43:26 +0530

    Add useDispatch hook, add function to add todo on pressing enter key

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const dispatch = useDispatch()
 
   const handleTextChange = (event) => {
     setText(event.target.value)
+  }
+
+  const handleKeyDown = (event) => {
+    const trimmedText = event.target.value.trim()
+
+    if (event.which === 13 && trimmedText) {
+      dispatch({ type: 'todos/todoAdded', payload: trimmedText })
+      setText('')
+    }
   }
 
   return (
@@ -12,8 +23,10 @@ const Header = () => {
       <input
         className="new-todo"
         placeholder="What needs to be done?"
+        autoFocus={true}
         value={text}
         onChange={handleTextChange}
+        onKeyDown={handleKeyDown}
       />
     </header>
   )
